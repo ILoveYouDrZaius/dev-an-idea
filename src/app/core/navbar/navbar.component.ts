3f5d47438b0bc4e03a02bf57724b297e4fe731e6
ILoveYouDrZaius <iloveyoudoctorzaius@gmail.com>
2018-07-29 21:03:11 +0200

    Nombre del usuario logueado en la navbar

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -9,15 +9,18 @@ import { tap } from 'rxjs/operators';
 })
 export class NavbarComponent implements OnInit {
   title = 'DevAnIdea';
+  username: string = '';
 
   constructor(public afAuth: AngularFireAuth) { }
 
   ngOnInit() {
     this.afAuth.authState
       .pipe(
-        tap((status)=>{
+        tap((status) => {
           if (status) {
             console.log('User logged in');
+            console.log(status.uid);
+            this.username = status['displayName'];
           } else {
             console.log('User not logged in');
           }
